df2d757ee17c22fcbace639e13ce928b67344968
yinghesun <pro_2011@163.com>
2018-06-05 14:42:16 +0800

    state

---
+++ backend/controllers/CheckStepOneController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yinghesun
+ * Date: 2018/6/4
+ * Time: 上午10:34
+ */
+
+namespace backend\controllers;
+
+
+class CheckStepOneController extends BaseController
+{
+
+}
