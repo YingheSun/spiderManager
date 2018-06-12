2ebc9a1fe13f70d4c01a4b0dd0d5f25d2492a944
zhangwanlong <zhangwanlong@sunlands.com>
2018-06-12 14:45:10 +0800

    修改配置

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,12 +1,17 @@
 <?php
+$db_host = getenv('DB_SIGNUP_HOST');
+$db_name = getenv('DB_SIGNUP_NAME');
+$db_user = getenv('DB_SIGNUP_USER');
+$db_pass = getenv('DB_SIGNUP_PASS');
 return [
     'components' => [
         'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'class'       => "yii\db\Connection",
+            'dsn'         => "mysql:host={$db_host};dbname={$db_name}",
+            'username'    => "{$db_user}",
+            'password'    => "{$db_pass}",
             'charset' => 'utf8',
+            'tablePrefix' => 'yii_',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
